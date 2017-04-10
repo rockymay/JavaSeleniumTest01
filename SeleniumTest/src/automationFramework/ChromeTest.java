a4b2bb4b77827545cc13efde6243695a6d0d2b4a
rockymay <rockymay@rocky>
2017-04-10 17:00:03 +1200

    Add Edit Auto Works, but Failed?

--- SeleniumTest/src/automationFramework/ChromeTest.java
+++ SeleniumTest/src/automationFramework/ChromeTest.java
@@ -1,4 +1,6 @@
 package automationFramework;
+import java.util.*;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -33,7 +35,8 @@ public class ChromeTest {
 		*/
 		String usernameInput = "Jiya";
 		String passwordInput = "Jiya@345";
-
+		List<String> addNewRecord = Arrays.asList("sup1", "sup2", "sup3", "sup4");
+		List<String> editRecord = Arrays.asList("Editsup1", "Editsup2", "Editsup3", "Editsup4");
         
         //Define IWebDriver
         driver.navigate().to("https://demo.econz.co.nz:1000/AdminPortal/Account/Login/exptest");
@@ -64,15 +67,143 @@ public class ChromeTest {
 
         
         driver.findElement(By.xpath("//*[@id='grid']/div[1]/a")).click();
-
+        
         //TextBox input
-        driver.findElement(By.name("BTN_NAME")).sendKeys("");
-        driver.findElement(By.name("BTN_DISPLAY_TITLE")).sendKeys("");
-        driver.findElement(By.name("PRE_CONDITION")).sendKeys("");
-        driver.findElement(By.name("VALUE_UPDATES")).sendKeys("");
+        driver.findElement(By.name("BTN_NAME")).sendKeys(addNewRecord.get(0));
+        driver.findElement(By.name("BTN_DISPLAY_TITLE")).sendKeys(addNewRecord.get(1));
+        driver.findElement(By.name("PRE_CONDITION")).sendKeys(addNewRecord.get(2));
+        driver.findElement(By.name("VALUE_UPDATES")).sendKeys(addNewRecord.get(3));
         Thread.sleep(3000);
+        
+        DropdownSelect(driver);
+        
+        driver.findElement(By.xpath("/html/body/div[6]/div[2]/div/div[27]/a[1]")).click();
+        driver.switchTo().alert().dismiss();
+        
+        driver.navigate().refresh();
+        //Use VerifyResult to Verify Record Change
+        VerifyResult(driver, addNewRecord, "Add New Record", "AddUpdate");
+        
+        
+      //########Edit Button Test From Here########
+        
+        //Select the New Record
+        Filter(driver, addNewRecord.get(0));
+        driver.findElement(By.xpath("//*[@id='grid']/div[4]/table/tbody/tr[1]/td[14]/a[1]")).click();
+
+
+        //Text box Update
+        driver.findElement(By.name("BTN_NAME")).clear();
+        driver.findElement(By.name("BTN_DISPLAY_TITLE")).clear();
+        driver.findElement(By.name("PRE_CONDITION")).clear();
+        driver.findElement(By.name("VALUE_UPDATES")).clear();
+        driver.findElement(By.name("BTN_NAME")).sendKeys(editRecord.get(0));
+        driver.findElement(By.name("BTN_DISPLAY_TITLE")).sendKeys(editRecord.get(1));
+        driver.findElement(By.name("PRE_CONDITION")).sendKeys(editRecord.get(2));
+        driver.findElement(By.name("VALUE_UPDATES")).sendKeys(editRecord.get(3));
+
+
+        driver.findElement(By.cssSelector("body > div.k-widget.k-window > div.k-popup-edit-form.k-window-content.k-content > div > div.k-edit-buttons.k-state-default > a.k-button.k-button-icontext.k-grid-update")).click();
+    
+
+
+        VerifyResult(driver, editRecord, "Edit Existing Record", "AddUpdate");
+
+
+        
         driver.close();
 		
 	}
+	public static void Filter(WebDriver driver, String text) throws InterruptedException
+	{
+		 driver.findElement(By.xpath("//*[@id='grid']/div[3]/div/table/thead/tr/th[1]/a[1]/span")).click();
+         WebElement Newdriver = driver.findElement(By.xpath("/html/body/div[5]/form/div[1]/input[1]"));
+         Thread.sleep(1000);
+         Newdriver.clear();
+         Newdriver.sendKeys(text);
+         driver.findElement(By.xpath("/html/body/div[5]/form/div[1]/div[2]/button[1]")).click();
+	
+	}
+	public static void DropdownSelect(WebDriver driver)
+	{
+		//Random rnd = new Random();
+        //define Double Click Functions
+		
+		
+		 driver.findElement(By.cssSelector("body > div.k-widget.k-window > div.k-popup-edit-form.k-window-content.k-content > div > div:nth-child(6) > span > span > span.k-input")).click();
+         //int optionNum = (driver.findElement(By.cssSelector("body > div.k-animation-container.km-popup > div > ul"))).findElements(By.className("k-item")).size;
+         //String ButtonLogoCSS = "body > div.k-animation-container.km-popup > div > ul > li:nth-child(" + (rnd.nextInt(optionNum)+1) + ")";
+         driver.findElement(By.cssSelector("body > div.k-animation-container.km-popup > div > ul > li:nth-child(5)")).click();
+         
+       //NextScreen
+         driver.findElement(By.cssSelector("body > div.k-widget.k-window > div.k-popup-edit-form.k-window-content.k-content > div > div:nth-child(10) > span > span > span.k-input")).click();
+         //string NextScreenXPath = "//*[@id='NEXT_SCREEN_DBID_listbox']/li[" + (rnd.Next(2, optionNum)) + "]";
+         driver.findElement(By.xpath("//*[@id='NEXT_SCREEN_DBID_listbox']/li[8]")).click();
+         
+       //NextState
+         driver.findElement(By.cssSelector("body > div.k-widget.k-window > div.k-popup-edit-form.k-window-content.k-content > div > div:nth-child(14) > span > span > span.k-input")).click();
+         //string NextStateXPath = "//*[@id='NEXT_STATE_DBID_listbox']/li[" + (rnd.Next(2, optionNum)).ToString() + "]";
+         driver.findElement(By.xpath("//*[@id='NEXT_STATE_DBID_listbox']/li[5]")).click();
 
+         //PendingState
+         driver.findElement(By.cssSelector("body > div.k-widget.k-window > div.k-popup-edit-form.k-window-content.k-content > div > div:nth-child(16) > span > span > span.k-input")).click();
+         //string PendingStateXPath = "//*[@id='PENDING_STATE_DBID_listbox']/li[" + (rnd.Next(2, optionNum)).ToString() + "]";
+         driver.findElement(By.xpath("//*[@id='PENDING_STATE_DBID_listbox']/li[5]")).click();
+
+         //EntityType
+         driver.findElement(By.cssSelector("body > div.k-widget.k-window > div.k-popup-edit-form.k-window-content.k-content > div > div:nth-child(18) > span > span > span.k-input")).click();
+         //int optionNumEntityType = (driver.FindElement(By.CssSelector("#ETT_DBID_listbox"))).FindElements(By.ClassName("k-item")).Count;
+         //string EntityTypeCSS = "#ETT_DBID_listbox > li:nth-child(" + (rnd.Next(2, optionNumEntityType)).ToString() + ")";
+         driver.findElement(By.cssSelector("#ETT_DBID_listbox > li:nth-child(5)")).click();
+         
+	}
+	public static void VerifyResult(WebDriver driver, List<String> newValue, String message, String AddorDel) throws InterruptedException
+	{
+		 //Filter
+        Filter(driver, newValue.get(0));
+
+        switch  (AddorDel)
+        {
+            case "Del":
+               String expectMessage = "No items to display";
+               // Thread.Sleep(500);
+                String actualMessagge = driver.findElement(By.cssSelector("#grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-info.k-label")).getText();
+                if (expectMessage==actualMessagge)
+                {System.out.println(message + " function test passed!");}
+
+                else
+                {
+                    for (int i = 1; i < 2; i++)
+                    {
+                    	String resultBtnNm = driver.findElement(By.xpath("//*[@id='grid']/div[4]/table/tbody/tr[1]/td[1]")).getText();
+                    	String resultBtnTt = driver.findElement(By.xpath("//*[@id='grid']/div[4]/table/tbody/tr[1]/td[2]")).getText();
+                    	String resultPreCdt = driver.findElement(By.xpath("//*[@id='grid']/div[4]/table/tbody/tr[1]/td[4]")).getText();
+                    	String resultVaUpdt = driver.findElement(By.xpath("//*[@id='grid']/div[4]/table/tbody/tr[1]/td[6]")).getText();
+
+                        if (resultBtnNm == newValue.get(0) && resultBtnTt == newValue.get(1) && resultPreCdt == newValue.get(2) && resultVaUpdt == newValue.get(3))
+                        {
+                        	System.out.println(message + " function test FAILED!!!!!");
+                        	System.out.println("There might be duplicate records, delete them and retry again");
+                        }
+                        else{ System.out.println(message + " function test pass!!!!!");}
+                    }
+                }
+                break;
+
+            case ("AddUpdate"):
+            	String result1 = driver.findElement(By.xpath("//*[@id='grid']/div[4]/table/tbody/tr/td[1]")).getText();
+            	String result2 = driver.findElement(By.xpath("//*[@id='grid']/div[4]/table/tbody/tr/td[2]")).getText();
+            	String result3 = driver.findElement(By.xpath("//*[@id='grid']/div[4]/table/tbody/tr/td[4]")).getText();
+            	String result4 = driver.findElement(By.xpath("//*[@id='grid']/div[4]/table/tbody/tr/td[6]")).getText();
+                
+                if (result1 == newValue.get(0) && result2 == newValue.get(1) && result3 == newValue.get(2) && result4 == newValue.get(3))
+                { System.out.println(message + " function test passed!"); }
+                else{ System.out.println(message + " function test FAILED!"); }
+                break;
+
+            default:break;
+        }
+        driver.navigate().refresh();
+    }
 }
+
